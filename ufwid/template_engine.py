f72b4e06ab2066a96e3a4cd42e5861368c94bd33
Krukov <glebov.ru@gmail.com>
2015-02-25 18:49:47 +0300

    Tepmlate engine begining

--- ufwid/template_engine.py
+++ ufwid/template_engine.py
@@ -2,21 +2,53 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
 
 
-class TemplateEngine(six.metaclass(ABCMeta)):
+from lxml import etree
+
+import urwid
+import six
+
+
+class BaseTemplate(six.add_metaclass(ABCMeta)):
+	 
+	def __init__(self, template):
+		self._template = template
 
 	@abstractmethod
-	def render(self, template):
-		pass
+	def render(self):
+		for element in self._get_iter_elements():
+			self._parse_element(element)
 
 	@abstractmethod
-	def parse(self, template):
-		pass
-
 	def _parse_element(self, element):
 		pass
 
+	@abstractmethod
 	def _get_element_paremetrs(self, element):
 		pass
 
-	# Template example
-	# <body>
+
+class Element(six.add_metaclass(ABCMeta))
+	
+	proxy = urwid.Button
+
+	def __init__(self, obj):
+		self.element = obj
+
+class Button(Element):
+	
+	
+
+class HtmlTemplate(BaseTemplate):
+	_map = {
+		'button': (urwid.Button, {'label': 'text', }), 
+		'a': urwid.Button, 
+		'dev': None, 
+		'form': None,
+	}
+
+	def render(self):
+		template = etree.fromstring(self._template)
+
+
+	def _parse_element(self, element):
+
